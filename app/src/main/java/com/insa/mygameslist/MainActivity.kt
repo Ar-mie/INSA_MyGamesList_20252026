9f2369219fa7edbba28fd2856e6bffa20dc34e0c
Emie Arlin <emie.arlin@insa-rennes.fr>
2026-02-06 16:56:37 +0100

    TP 2 affichage des jeux et leurs genres en liste

--- app/src/main/java/com/insa/mygameslist/MainActivity.kt
+++ app/src/main/java/com/insa/mygameslist/MainActivity.kt
@@ -4,23 +4,71 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.ImageLoader
+import coil3.compose.AsyncImage
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import com.insa.mygameslist.data.Game
 import com.insa.mygameslist.data.IGDB
 import com.insa.mygameslist.ui.theme.MyGamesListTheme
+import kotlinx.datetime.format.Padding
+
+//@Preview(showBackground = true)
+@Composable
+fun AfficherJeux(innerPadding: PaddingValues, igdb: IGDB){
+
+    LazyColumn(modifier = Modifier.padding(innerPadding)) {
+        items(igdb.games) { game ->
+            AfficheDonneesJeu(game)
+
+        }
+    }
+}
+
+@Composable
+fun AfficheDonneesJeu(game: Game){
+    Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+        val coverId: Long = game.cover
+        AsyncImage(
+            model = "https:${IGDB.coversMap.get(coverId)}",
+            contentDescription = null
+        )
+        Column(){
+                Text(game.name)
+                Text("Genres :")
+                Row(){
+                    game.genres.forEach { genreId ->
+                        IGDB.genresMap.get(genreId)?.let { Text("${it}") } }
+
+                }
+            }
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +83,7 @@ class MainActivity : ComponentActivity() {
                     topBar = {
                         TopAppBar(
                             colors = topAppBarColors(
-                                containerColor = Color.Magenta,
+                                containerColor = Color.Cyan,
                                 titleContentColor = Color.Black,
                             ),
                             title = { Text("My Games List") })
@@ -43,9 +91,8 @@ class MainActivity : ComponentActivity() {
                     contentWindowInsets = WindowInsets.systemBars,
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
-
+                    //Text("À remplir", modifier = Modifier.padding(innerPadding))
+                    AfficherJeux(innerPadding, IGDB)
                 }
             }
         }
--- app/src/main/java/com/insa/mygameslist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygameslist/data/IGDB.kt
@@ -8,6 +8,12 @@ import com.insa.mygameslist.R
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var coversMap: Map<Long,String>
+    lateinit var games: List<Game>
+    lateinit var genres: List<Genre>
+    lateinit var genresMap: Map<Long,String>
+    lateinit var platformLogo: List<PlatformLogo>
+    lateinit var platform: List<Platform>
 
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
@@ -16,7 +22,44 @@ object IGDB {
         )
 
         covers = coversFromJson
+        coversMap = covers.associateBy({ it.id },{it.url})
+
+        val gamesFromJson: List<Game> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Game>>() {}.type
+        )
+
+        games = gamesFromJson
+
+        val genresFromJson: List<Genre> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genre>>() {}.type
+        )
+
+        genres = genresFromJson
+        genresMap = genres.associateBy({ it.id },{it.name})
+
+
+        val platLogoFromJson: List<PlatformLogo> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<PlatformLogo>>() {}.type
+        )
+
+        platformLogo = platLogoFromJson
+
+        val platformFromJson: List<Platform> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<Platform>>() {}.type
+        )
+
+        platform = platformFromJson
+
     }
+
 }
 
 data class Cover(val id: Long, val url: String)
+data class Game(val id:Long, val cover:Long, val firstRealease:Long, val genres:List<Long>, val name: String, val platform: List<Long>, val summary:String, val totalRating:Float)
+data class Genre(val id:Long, val name: String)
+data class PlatformLogo(val id:Long, val url:String)
+data class Platform(val id:Long,val name:String,val platLogo:Long)
